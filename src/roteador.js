bf5790abc7297e55ea3ee9ff06efc16b7f37cc98
Rafael Maurício <rafapmauricio@gmail.com>
2023-09-06 18:55:32 -0300

    atualização na detalhação de pokemon

--- src/roteador.js
+++ src/roteador.js
@@ -5,6 +5,6 @@ const {
 } = require("./controladores/pokemon");
 
 rotas.get("/pokemon/:pagina", listagemPokemonPorPagina);
-rotas.get("/pokemon/:idOuNome", pokemonPorNomeOuId);
+rotas.get("/pokemon/detalhes/:idOuNome", pokemonPorNomeOuId);
 
 module.exports = rotas;
